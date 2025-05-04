ae94f14b9f9adc3b1018939b44f6bfea55598fe5
Altan <uralaltan10@gmail.com>
2025-05-04 16:52:54 +0300

    Minor removal

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,9 +1,9 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/homeScreen';
 import BleScreen from '../screens/bleScreen';
-import { RootStackParamList } from '../types/type.d'; // Import the param list type
+import {RootStackParamList} from '../types/type.d';
 
-const Stack = createNativeStackNavigator<RootStackParamList>(); // Use the param list type
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
@@ -17,7 +17,7 @@ const RootStack = createNativeStackNavigator({
     Ble: {
       screen: BleScreen,
       options: {
-        headerShown: false, // Keep header hidden for now, can be customized later
+        headerShown: false,
       },
     },
   },
